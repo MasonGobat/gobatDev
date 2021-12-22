5a4e343dc57f52d475accd2f0ef8471fdf00863b
Mason Gobat <masongobat@gmail.com>
2021-12-22 16:38:29 -0500

    changed header for changed page file types

--- Scripts/csi.js
+++ Scripts/csi.js
@@ -3,8 +3,8 @@ function displayHeader() {
     document.write('<h1 id="logo"><a href="index.html" class="internalLink">Gobat.dev</a></h1>');
     document.write('<div id="menuButtons">');
     document.write('<h2 class="animatedButton"><a href="index.html" class="internalLink">Home</a></h2>');
-    document.write('<h2 class="animatedButton"><a href="mason.xhtml" class="internalLink">Mason Gobat</a></h2>');
-    document.write('<h2 class="animatedButton"><a href="makenna.xhtml" class="internalLink">Makenna Fitzsimmons</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="mason.html" class="internalLink">Mason Gobat</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="makenna.html" class="internalLink">Makenna Fitzsimmons</a></h2>');
     document.write('</div>');
     document.write('</div>');
 }
