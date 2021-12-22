3951dff33b1ea2601641f1858a6c3b0c0798e216
Mason Gobat <masongobat@gmail.com>
2021-12-22 16:19:52 -0500

    created a client side includes function

---
+++ Scripts/csi.js
@@ -0,0 +1,12 @@
+function displayHeader() {
+    document.write('<div id="navMenu">');
+    document.write('<h1 id="logo"><a href="index.html" class="internalLink">Gobat.dev</a></h1>');
+    document.write('<div id="menuButtons">');
+    document.write('<h2 class="animatedButton"><a href="index.html" class="internalLink">Home</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="mason.xhtml" class="internalLink">Mason Gobat</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="makenna.xhtml" class="internalLink">Makenna Fitzsimmons</a></h2>');
+    document.write('</div>');
+    document.write('</div>');
+}
+
+displayHeader();
