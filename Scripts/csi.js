a0ec81854decbbbe209a1457fb30ac794656cdd2
Mason Gobat <masongobat@gmail.com>
2022-01-19 10:25:26 -0500

    added navButton class to nav buttons

--- Scripts/csi.js
+++ Scripts/csi.js
@@ -1,10 +1,10 @@
 function displayHeader() {
     document.write('<div id="navMenu">');
-    document.write('<h1 id="logo"><a href="index.html" class="internalLink">Gobat.dev</a></h1>');
+    document.write('<h1 id="logo"><a href="index.html" class="navButton">Gobat.dev</a></h1>');
     document.write('<div id="menuButtons">');
-    document.write('<h2 class="animatedButton"><a href="index.html">Home</a></h2>');
-    document.write('<h2 class="animatedButton"><a href="mason.html">Mason Gobat</a></h2>');
-    document.write('<h2 class="animatedButton"><a href="makenna.html">Makenna Fitzsimmons</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="index.html" class="navButton">Home</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="mason.html" class="navButton">Mason Gobat</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="makenna.html" class="navButton">Makenna Fitzsimmons</a></h2>');
     document.write('</div>');
     document.write('</div>');
 }
