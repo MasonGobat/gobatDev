d8a39801110b8a4cd1ed0bf9725082bd8d63ec63
Mason Gobat <masongobat@gmail.com>
2022-02-28 10:49:27 -0500

    changed the name of the third tab

--- Scripts/csi.js
+++ Scripts/csi.js
@@ -2,9 +2,9 @@ function displayHeader() {
     document.write('<div id="navMenu">');
     document.write('<h1 id="logo"><a href="index.html" class="navButton">Gobat.dev</a></h1>');
     document.write('<div id="menuButtons">');
-    document.write('<h2 class="animatedButton"><a href="index.html" class="navButton">Home</a></h2>');
-    document.write('<h2 class="animatedButton"><a href="mason.html" class="navButton">Mason Gobat</a></h2>');
-    document.write('<h2 class="animatedButton"><a href="makenna.html" class="navButton">Makenna Fitzsimmons</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="index.html" class="navButton home">Home</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="mason.html" class="navButton mason">Mason Gobat</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="essays.html" class="navButton essay">Essays</a></h2>');
     document.write('</div>');
     document.write('</div>');
 }
