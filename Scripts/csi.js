25b2a200b1ea2b59c7c754f8d68e1e04a459946b
MasonGobat <masongobat@gmail.com>
2022-12-05 02:57:16 -0500

    changed wording and link to reflect new 'essays' page

--- Scripts/csi.js
+++ Scripts/csi.js
@@ -4,7 +4,7 @@ function displayHeader() {
     document.write('<div id="menuButtons">');
     document.write('<h2 class="animatedButton"><a href="index.html" class="navButton home">Home</a></h2>');
     document.write('<h2 class="animatedButton"><a href="mason.html" class="navButton mason">Mason Gobat</a></h2>');
-    document.write('<h2 class="animatedButton"><a href="essays.html" class="navButton essay">Essays</a></h2>');
+    document.write('<h2 class="animatedButton"><a href="projects.html" class="navButton essay">Projects</a></h2>');
     document.write('</div>');
     document.write('</div>');
 }
